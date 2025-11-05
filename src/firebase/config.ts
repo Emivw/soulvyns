80c3ad331622eef6db65788ff1d3ff7a033fc78a
Emile van Wyk <emiya.vanwyk@gmail.com>
2025-11-05 13:19:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6524401861-cc106",
+  "appId": "1:744606621171:web:0bdb19d13e34ef039bc686",
+  "apiKey": "AIzaSyBxcyoIJ3SoDuCJeN2UGXV1ce4_y6CHVG4",
+  "authDomain": "studio-6524401861-cc106.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "744606621171"
+};
